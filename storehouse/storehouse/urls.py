1d8e76f8be936eb539e85c5fda09ec3eca5e5f23
iamin <d.aminkafi@gmail.com>
2025-01-17 20:35:12 +0330

    Admin site header modified

--- storehouse/storehouse/urls.py
+++ storehouse/storehouse/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'I4 Lab Storehouse'
+admin.site.site_title = 'I4 Lab Storehouse Admin Site'
+admin.site.index_title = 'Admin Site'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
